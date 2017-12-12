913d73ecaaebba8c218b477460388d6ecbc8cfd7
Steven Vispoel <steven.vispoel@aquafin.be>
2017-12-12 17:01:08 +0100

    Added BAM modulation en finished Multiplexer loop

--- software/controller/core.py
+++ software/controller/core.py
@@ -1,9 +1,9 @@
 """--------------IMPORT MODULES--------------"""
 import RPi.GPIO as GPIO
-import threading
-from time import sleep
+import datetime
+import array
 
-#code designed for a 5 * 8 christmas tree
+#code designed for a 7 * 8 christmas tree
 
 #these are the core functions, imported to LEDcube.py, which contains the
 #   main program operations
@@ -23,6 +23,7 @@ pins = [
     23, #pin 11, shift register SPI CLOCK
     24, #pin 8, shift register SPI LATCH
     19, #pin 10, shift register SPI SERIAL DATA
+    29 #pin 5, output_enabled
 ]
 
 transistors = [11, 13, 15]
@@ -38,62 +39,67 @@ points = [[[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [
           [[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0]],
           [[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0]],
           [[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0]],
-          [[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0]]]
+          [[0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x0], [0x0, 0x0, 0x5]]]
+
+bam = []
+elements = 168
+elements_per_layer = 8
 
 """-------------CLASS DEFINITIONS----------------"""
 #this class handles interactions between the code and the physical shift registers
 class ShiftRegister():
-    def __init__(self, datapin, clockpin, latchpin):
+    def __init__(self, datapin, clockpin, latchpin, output_enablepin):
         self.datapin = datapin
         self.clockpin = clockpin
         self.latchpin = latchpin
+        self.output_enablepin = output_enablepin
 
-    def clock(self, n):
-        #input data
-        GPIO.output(self.datapin, n)
-        
-        #clock clock (whee)
-        GPIO.output(self.clockpin, 1)
-        GPIO.output(self.clockpin, 0)
-        
-        #reset data pin
-        GPIO.output(self.datapin, 0)
+    def send(self, bit):
+        print(bit, end="")
+        # TODO SPI interface
 
-    def latch(self):
-        #latch and reset register
-        GPIO.output(self.latchpin, 1)
-        GPIO.output(self.latchpin, 0)
+class Bam_filler:
+    def __init__(self):
+        self.layers_bam = []
 
-    def clear(self):
-        #clear and reset register by filling with low values
-        for i in range(16):
-            self.clock(0)
-        self.latch()
+    def fill(self):
+        self.layers_bam = []
+        for layer_index in range(len(points)):
+            for led_index in range(len(points[layer_index])):
+                for rgb_index in range(len(points[layer_index][led_index])):
+                    for byte_index in range(4):
+                        mask = 1 << byte_index
+                        rgb_bit = (points[layer_index][led_index][rgb_index] & mask) >> byte_index
+                        for count in range(2**byte_index):
+                            self.layers_bam.append(rgb_bit)
+        global bam
+        bam = self.layers_bam
 
-#this class runs in a separate thread, continuously reading the 'points' list
+
+#this class runs in a separate thread, continuously reading the 'bam' list
 #   and writing its values to the LED cube
-class Multiplexer():
+
+
+class Multiplexer:
     def __init__(self):
         self.running = True
-        self.register = ShiftRegister(19, 23, 24)
+        self.register = ShiftRegister(19, 23, 24, 29)
+        self.bam_filler = Bam_filler()
 
     #the second parameter has to exist for the thread to run, but does nothing
-    def multiplex(self, p):
+    def multiplex(self):
         layer = 0x0
-        while self.running:
-            for bam_cycle in range(16):
-                for led in range(len(points)):
+        # while self.running:
+        self.bam_filler.fill()
+        starttime = datetime.now()
+        for bam_cycle in range(1):
+            for element in range(elements):
+                if element % elements_per_layer == 0:
                     self.update_layer(layer)
-                    #turn previous layer off before updating LEDs
-                    GPIO.output(transistors[y-1], 0)
-                    #pick which register to write to - each controls half
-                    #   of the cube
-                    self.register2.clock(points[y][x][z])
-                    #write register values to cube and enable layer
-                    self.register1.latch()
-                    layer += 1
-                    sleep(0.001)
-                layer = 0x0
+                self.register.send(bam[(element * 15) + bam_cycle])
+            layer += 1
+        layer = 0x0
+        print("Timedelta: " + str(datetime.now().microsecond - starttime.microsecond))
 
     def update_layer(self, layer):
         for transistor_index in range(len(transistors)):
